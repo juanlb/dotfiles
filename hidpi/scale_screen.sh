f13faf0ed43e29eccb18439046aef7525cd1a591
Juan La Battaglia <juan.labattaglia@snappler.com>
2018-04-28 23:36:55 -0300

    corrigiendo escala

--- hidpi/scale_screen.sh
+++ hidpi/scale_screen.sh
@@ -4,10 +4,10 @@ EXTERNO=$(xrandr --query | grep "^DP1 connected")
 
 if [ -z "$EXTERNO" ]
 then
-gsettings set org.gnome.desktop.interface text-scaling-factor 1.4
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.3
 echo "Monitor no conectando, usando escala de 1.4 para display 13' "
 else
-gsettings set org.gnome.desktop.interface text-scaling-factor 1.4
+gsettings set org.gnome.desktop.interface text-scaling-factor 1
 echo "Monitor CONECTADO, usando escala de 1 para no espiar la pantalla :-P"
 fi
 
